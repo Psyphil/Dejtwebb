ac8262b1dabb5c06608a3cee0621f0690fdf1cbf
Filip Axbrink Jönsson <filip@axbrink.com>
2017-12-18 15:31:14 +0100

    models

---
+++ DejtWebb/DejtWebb/Models/user.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DejtWebb.Models
+{
+    public class User
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public int Id { get; set; }
+        public List<User> Friends { get; set; }
+
+        public User()
+        {
+
+        }
+
+    }
+}
