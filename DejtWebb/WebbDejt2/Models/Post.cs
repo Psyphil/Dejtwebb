6b395ee57dffcb2198a88563e8b65fa1785993a5
Filip Axbrink Jönsson <filip@axbrink.com>
2017-12-20 10:26:34 +0100

    redo

---
+++ DejtWebb/WebbDejt2/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebbDejt2.Models
+{
+    public class Post 
+    {
+        public string Text { get; set; }
+        public int Id { get; set; }
+        public ApplicationUser Author { get; set; }
+        public ApplicationUser Receiver { get; set; }
+    }
+
+    //////public class PostDbContext : IdentityDbContext<ApplicationUser>
+    //////{
+    //////    public PostDbContext()
+    //////    {
+
+    //////    }
+
+    //////}
+}
